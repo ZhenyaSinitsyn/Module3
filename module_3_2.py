30a060c0b81aa76c271fd071d5624332a85a05d8
ZhenyaSinitsyn <sinitsyn09@gmail.com>
2024-11-10 21:44:31 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,16 @@
+def send_email(mes, rec, sender = 'university.help@gmail.com'):
+    if (("@" and (".com" or ".ru" or ".net")) not in (rec or sender)
+    or (("@" or (".com" or ".ru" or ".net")) not in (rec or sender))):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {rec}')
+    elif rec == sender:
+        print('Нельза отправить письмо самому себе')
+    elif sender == 'university.help@gmail.com':
+        print('Пиисьмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    else:
+        print('Нестандартный отправитель! Письмо отправлено с адреса {sender} на адрес {recipient}')
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender = 'urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender = 'urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender = 'urban.teacher@mail.ru')
